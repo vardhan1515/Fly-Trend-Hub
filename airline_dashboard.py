667fad7d2138cf51595654b63f79b138514348ac
unknown <vardhanburande2002@gmail.com>
2025-01-20 19:18:52 -0500

    fly_trend_hub

--- airline_dashboard.py
+++ airline_dashboard.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import sqlite3
+import base64
 
 # Load background image and encode it
 if "background_image" not in st.session_state:
